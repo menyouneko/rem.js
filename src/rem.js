8671071baea49f24e715351c382fb839c4ccb1c2
ChiHo-Ng <ngchiho04@gmail.com>
2017-04-14 11:52:39 +0800

    Add babel.

--- src/rem.js
+++ src/rem.js
@@ -1,29 +1,28 @@
-/**
+/*
  * @Author: ChiHoNg - A Rookie FE
  * @E-mail: sheep_zh@163.com
  * @Date:   2017-02-18 15:41:40
  * @Last Modified by:   ChiHo-Ng
- * @Last Modified time: 2017-07-05 17:59:15
+ * @Last Modified time: 2017-07-05 18:00:25
  */
 (function (window) {
-  var rem = (function () {
+  'use strict';
+  let rem = (function () {
     /**
-     * @param  {Node} remEl
+     * 传入meta节点,解析meta节点的内容,返回一个解析后的对象
+     * @param  {Node} remEl 
      * @return {Object}
+     *
      * @example
-     * parseRem(document.querySelector('meta[name="rem-setting"]'))
-     * // => {designWidth: "750", maxWidth: "500", ratio: "2", dpr: "1"}
+     * parseRem(document.querySelector('meta[name = "rem-setting"'))
+     * return {designWidth: "750", maxWidth: "500", ratio: "2", dpr: "1"}
      */
     function parseRem(remEl) {
-      // 获取content内容
-      var remContent = remEl.getAttribute('content');
-      // 解析content，返回一个Object
-      var obj = {};
-      remContent.split(',').map(function (item) {
-        var arr = item.split('=').map(function (item) {
-          return item.replace(/^\s+|\s+$/g, '')
-        });
-        arr[0] = arr[0].replace(/\-+(\w{1})/, function (item, $1) {
+      let remContent = remEl.getAttribute('content');
+      let obj = {};
+      remContent.split(',').map((item) => {
+        let arr = item.split('=').map((item) => item.replace(/^\s+|\s+$/g, ''));
+        arr[0] = arr[0].replace(/\-+(\w{1})/, (item, $1) => {
           return $1.toUpperCase();
         });
         obj[arr[0]] = arr[1];
@@ -32,10 +31,14 @@
       return obj;
     }
 
+    /**
+     * 设置meta标签,设置viewport的内容
+     * @param {Object} option
+     */
     function setViewport(option) {
-      var viewport = document.querySelector('meta[name="viewport"]');
-      var dpr = option.dpr || 1; // 默认dpr = 1
-      var content = 'width=device-width, initial-scale=' + 1 / dpr + ', minimum-scale=' + 1 / dpr + ', maximum-scale=' + 1 / dpr + ', user-scalable=no';
+      let viewport = document.querySelector('meta[name = "viewport"]');
+      let { dpr = 1 } = option; // 默认dpr = 1
+      let content = 'width=device-width, initial-scale=' + 1 / dpr + ', minimum-scale=' + 1 / dpr + ', maximum-scale=' + 1 / dpr + ', user-scalable=no';
       if (viewport) {
         viewport.setAttribute('content', content);
       } else {
@@ -45,7 +48,8 @@
         document.head.appendChild(viewport);
       }
     }
-    /**
+
+    /** 
      * 视口宽度 clientWidth
      * 设计稿 designWidth
      * 转换系数 ratio（ratio表示将设计稿的px单位除以ratio，得出的值直接用于rem即可）
@@ -56,52 +60,56 @@
      * 根据以上公式就可以计算出rem应为多少
      * 获取视口宽度
      * 此处获得的clientWidth是计算了dpr后的值，所以无需再与dpr相乘
+     * @param {Object} option
+     * @return void
      */
     function setFontSize(option) {
-      var clientWidth = document.documentElement.clientWidth || window.clientWidth;
-      var designWidth = option.designWidth || 750;
-      var ratio = option.ratio || 100;
-      var maxWidth = option.maxWidth || 500;
-      var dpr = option.dpr || 1; // 默认值
-      // 判断当前宽度是否比设置的max-width大，是则使用设置的maxWidth，否则使用当前的clientWidth
-      clientWidth = clientWidth > maxWidth * dpr ? maxWidth * dpr : clientWidth;
+      let clientWidth = document.documentElement.clientWidth || window.clientWidth;
+      let { designWidth = 750, ratio = 100, maxWidth = 500, dpr = 1 } = option; // 默认值
+      clientWidth = clientWidth > maxWidth * dpr ? maxWidth * dpr : clientWidth; // 判断当前宽度是否比设置的max-width大，是则使用设置的maxWidth，否则使用当前的clientWidth
       document.documentElement.setAttribute('data-dpr', dpr);
       document.documentElement.style.fontSize = clientWidth * ratio / designWidth + 'px';
-      console.log(clientWidth * ratio / designWidth + 'px');
     }
 
+    /**
+     * px转换成rem
+     * @param  {Number|String} px
+     * @return {String}
+     */
     function px2rem(px) {
       px = px.toString().replace('px', '');
       return px / remSetting.ratio + 'rem';
     }
 
-    function rem2px(remUnit) {
-      remUnit = remUnit.toString().replace('rem', '');
-      var rootFontSize = document.documentElement.style.fontSize.replace('px', '');
-      return remUnit * rootFontSize;
+    /**
+     * rem转换成px
+     * @param  {Number|String} px
+     * @return {String}
+     */
+    function rem2px(rem) {
+      rem = rem.toString().replace('rem', '');
+      let rootFontSize = document.documentElement.style.fontSize.replace('px', '');
+      return rem * rootFontSize + 'px';
     }
 
-    // 获取自定义的rem meta标签
-    var remEl = document.querySelector('meta[name="rem-setting"]');
-    var remSetting = remEl && parseRem(remEl);
+    let remEl = document.querySelector('meta[name = "rem-setting"'); // 获取自定义的rem meta标签
+    let remSetting = remEl && parseRem(remEl);
     remSetting.dpr = remSetting.dpr || window.devicePixelRatio || 1; // 若自定义了dpr，就用自定义的值，否则用屏幕的dpr值
-    setViewport(remSetting);
-    setFontSize(remSetting);
-    // 事件绑定
-    // 缩放时重新设置font-size
-    window.addEventListener('resize', function () {
+
+    setViewport(remSetting); // 设置viewport
+    setFontSize(remSetting); // 设置root font-size
+
+    window.addEventListener('resize', function () { // 事件绑定,缩放时重新设置font-size
       clearTimeout(window.rem.tId);
-      // 函数节流
-      window.rem.tId = setTimeout(function () {
+      window.rem.tId = setTimeout(function () { // 函数节流
         setFontSize(remSetting);
       }, 100);
     }, false);
-    // 返回两个转换方法接口
-    return {
-      px2rem: px2rem,
-      rem2px: px2rem
+
+    return { // 返回两个转换方法接口
+      px2rem,
+      rem2px
     }
   })();
-  // 暴露给全局变量
-  window.rem = rem;
+  window.rem = rem; // 全局变量
 })(window);
